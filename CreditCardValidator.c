b42a2106ca6bb6ead3ee8a7b26965243ba8e8d9f
ColePriser <cpriser@purdue.edu>
2022-07-25 14:33:49 -0500

    Documentation and different promts

--- CreditCardValidator.c
+++ CreditCardValidator.c
@@ -2,12 +2,14 @@
 
 int main() {
     long credNum;
-    printf("Input your credit card number!\n");
+    printf("Input your credit card number below:\n");
     scanf("%ld\n", &credNum);
+
+    //Keeps asking for new card number until it is greater than 0
     while (credNum <= 0) {
-        printf("Invalid number! Try again!\n");
+        printf("Invalid number! Try again:\n");
         scanf("%ld\n", &credNum);
     }
-    
+
     return 0;
 }
