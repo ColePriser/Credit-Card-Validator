6ea6c767c1bb1853e99e72f4d4476fa1f7e6cbe2
ColePriser <cpriser@purdue.edu>
2022-07-31 23:53:13 -0500

    Odd number functionality of check sum

--- CreditCardValidator.c
+++ CreditCardValidator.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 
 int main() {
-    long credNum;
+    char credNum[16];
+
+    //Initial credit card input request
     printf("Input your credit card number below:\n");
-    scanf("%ld\n", &credNum);
+    scanf("%s\n", &credNum);
 
     //Keeps asking for new card number until it is greater than 0
     while (credNum <= 0) {
@@ -11,5 +13,20 @@ int main() {
         scanf("%ld\n", &credNum);
     }
 
-    return 0;
+    int checkSum = 0;
+
+    for (int x = 0; x < 16; x++) {
+        //Find check sum of odd indexes
+        //Just add every number at odd index
+        if (x % 2 == 1) {
+            checkSum += credNum[x];
+        } 
+        //Find check sum of even indexes
+        //Double every number at even index
+        //If this double is double digit, then add the two digits together.
+        //Then, add this addition to the check sum.
+        //If this double is single  digit, then add it to the check sum.
+        else {
+        }
+    }
 }
