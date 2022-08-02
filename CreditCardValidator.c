22ded380b7b7fd1c1c49b830313dc75971a6a398
ColePriser <cpriser@purdue.edu>
2022-08-02 01:17:19 -0500

    Separating tasks in main into function

--- CreditCardValidator.c
+++ CreditCardValidator.c
@@ -2,64 +2,59 @@
 #include <math.h>
 #include <stdbool.h>
 #include <string.h>
+#include <malloc.h>
+
+void validate(char *credCardNum) {
+    int checkSum = 0;
+    int length = strlen(credCardNum);
+    for (int x = length - 1; x >= 0; x--) {
+        //Find check sum of odd indexes
+        if (x % 2 == 1) {
+            //Just add every number at odd index
+            checkSum += (int) credCardNum[x];
+        } 
+        //Find check sum of even indexes
+        else {
+            //Double every number at even index
+            int doubleNum = 2 * (int) credCardNum[x];
+
+            //If doubleNum is double digit, then subtract 9 and add to check sum.
+            if (doubleNum > 9) {
+                doubleNum -= 9;
+                checkSum += doubleNum;
+            } else {
+                //If this double is single digit, then add it to the check sum.
+                checkSum += doubleNum;
+            }
+        }
+    }
+
+    //If the check sum divided by 10 has a remainder of 0, then this credit card number is valid
+    if (checkSum % 10 == 0) {
+        printf("Valid Number!\n");
+    } else {
+        printf("Invalid Number!\n");
+    }
+}
 
 int main() {   
     bool goAgain = false;
     do {
-        char credNum[16];
+        char *credNum= malloc(16);
 
         //Initial credit card input request
         printf("Input your credit card number below (NO SPACES):\n");
-        scanf("%s\n", &credNum);
-
-        //Keeps asking for new card number until it is greater than 0
-        while (credNum <= 0) {
-            printf("Invalid number! Try again:\n");
-            scanf("%ld\n", &credNum);
-        }
-
-        int checkSum = 0;
-
-        for (int x = 15; x >= 0; x--) {
-            //Find check sum of odd indexes
-            if (x % 2 == 1) {
-                //Just add every number at odd index
-                checkSum += credNum[x];
-            } 
-            //Find check sum of even indexes
-            else {
-                //Double every number at even index
-                int doubleNum = credNum[x];
-                doubleNum = credNum[x] * 2;
-
-                //If doubleNum is double digit, then add the two digits together.
-                if (doubleNum > 9) {
-                    int firstNum = doubleNum / pow(10, log10(doubleNum));
-                    int lastNum = doubleNum % 10;
-                    int addition = firstNum + lastNum;
-
-                    //Add this addition to the check sum.
-                    checkSum += addition;
-                } else {
-                    //If this double is single digit, then add it to the check sum.
-                    checkSum += doubleNum;
-                }
-            }
-        }
-
-        //If the check sum divided by 10 has a remainder of 0, then this credit card number is valid
-        if (checkSum % 10 == 0) {
-            printf("Valid credit card number!\n");
-        }
-        else {
-            printf("Invlid credit card number!\n");
-        }
+        scanf("%s\n", credNum);
+    
+        //Call validate function to see if number is valid
+        validate(credNum);
+        printf("validated...\n");
         bool correctInput = false;
         do {
             printf("Do you want to input a new credit card number?\n");
             printf("Type YES or NO:\n");
-            char *again;
-            scanf("%s", &again);
+            char *again = malloc(4);
+            scanf("%s", again);
             if (strcmp(again, "YES") == 0) {
                 goAgain = true;
                 correctInput = true;
@@ -70,4 +65,3 @@ int main() {
     } while (goAgain);
     return 0;
 }
-
